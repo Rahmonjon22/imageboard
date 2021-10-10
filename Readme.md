24051fa52ea8fd251fc7091e6384abb71d1fe5a4
Rahmonjon22 <rahmonjon2@yahoo.com>
2021-10-10 12:34:54 +0200

    backend

--- Readme.md
+++ Readme.md
@@ -4,6 +4,10 @@
   ## client folder
 npx create-react-app ./ 
 
+npm install axios moment react-file-base64 redux redux-thunk  => axios for api  moment for time react-file-base64 for image storage 
+delete src folder & create src file again plus create index js inside it
+
+
   ## server folder
 npm init -y  to create package.json
 npm install cors express mongoose
--- server/index.js
+++ server/index.js
@@ -1,3 +1,19 @@
 import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
+
+const app = express();
+app.use(express.json({limit: '30mb', extended: true}));
+app.use(express.urlencoded({limit: '30mb', extended: true}));
+
+app.use(cors());
+
+const CONNECTION_URL = "mongodb+srv://rahmonjon-ibragimov:Asaka6264@cluster0.9yhv3.mongodb.net/fullstack-imageboard?retryWrites=true&w=majority"
+
+
+const PORT = process.env.PORT || 5000;
+
+mongoose
+.connect(CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true })
+.then( ()=> app.listen(PORT, ()=> console.log(`Server Running on Port: http://localhost:${PORT}`)))
+.catch(error => console.log(`${error} did not connect`));
