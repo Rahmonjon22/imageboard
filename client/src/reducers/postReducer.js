b60f98c8023f5636a832d2af0b075077791bb4b9
Rahmonjon22 <rahmonjon2@yahoo.com>
2021-10-22 22:56:42 +0200

    push

--- client/src/reducers/postReducer.js
+++ client/src/reducers/postReducer.js
@@ -12,3 +12,30 @@ const postReducer = (post = [], action) => {
 };
 
 export default postReducer;
+app.post("/index/:id", function(req,res){
+    TestData.findById(req.params.id, function(err, theUser){
+        if(err){
+            console.log(err);
+        } else {
+            theUser.likes += 1;
+            theUser.save();
+            console.log(theUser.likes);
+        }
+    });
+});
+
+app.post("/index/:id", function(req,res){
+    TestData.findById(req.params.id, function(err, theUser){
+        if(err){
+            console.log(err);
+            return res.status(500).send('Something went wrong!'); // You should notify user about any error    
+        } else {
+            theUser.likes += 1;
+            theUser.save(function(err){
+            if(err) return res.status(500).send('Something went wrong!');
+            return res.send({likes_count: theUser.likes});
+            });
+  
+        }
+    });
+  });
